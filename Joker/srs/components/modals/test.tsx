74dac525d6085e409bdd705740280e5366cf2faf
Max <maskalyov.maks@gmail.com>
2022-12-17 13:11:33 +0300

    fix syntax

--- Joker/srs/components/modals/test.tsx
+++ Joker/srs/components/modals/test.tsx
@@ -1,33 +1,30 @@
-import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export interface TestModalProps {
-    onClose: () => void;
+  onClose: () => void;
 }
 
 export const TestModal = ({onClose}: TestModalProps) => {
-    return (
-        <View style={styles.modalContainer}>
-            <Text style={styles.text}>Hello, this is test modal for Joker!</Text>
-            <Button
-                onPress={onClose}
-                title='Close modal'
-            />
-        </View>
-    );
+  return (
+    <View style={styles.modalContainer}>
+      <Text style={styles.text}>Hello, this is test modal for Joker!</Text>
+      <Button onPress={onClose} title="Close modal" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        backgroundColor: 'orange',
-        justifyContent: 'space-around',
-        alignItems:'center',
-    },
-    text: {
-        fontSize: 25,
-        fontWeight: 'bold',
-        color:'white',
-        textAlign:'center',
-    }
-})
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'orange',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+});
