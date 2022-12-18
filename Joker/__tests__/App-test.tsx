96d66fc707e18aff4eb06c87a6c4904edd798a06
samandar <samandar8939522@gmail.com>
2022-12-18 13:55:08 +0500

    fix: lint

--- Joker/__tests__/App-test.tsx
+++ Joker/__tests__/App-test.tsx
@@ -5,11 +5,12 @@
 import 'react-native';
 
 import React from 'react';
+
 import renderer from 'react-test-renderer';
+
 import {App} from '../src/app';
 
 // Note: test renderer must be required after react-native.
-
 
 it('renders correctly', () => {
   renderer.create(<App />);
