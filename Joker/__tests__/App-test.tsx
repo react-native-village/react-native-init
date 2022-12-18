3e4e33260605327cd18c3f52a1280213182a6209
samandar <samandar8939522@gmail.com>
2022-12-18 13:51:13 +0500

    fix: merge and lint

--- Joker/__tests__/App-test.tsx
+++ Joker/__tests__/App-test.tsx
@@ -6,7 +6,7 @@ import 'react-native';
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from '../App';
+import {App} from '../src/app';
 
 // Note: test renderer must be required after react-native.
 
