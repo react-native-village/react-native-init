19e112b8308470a21a2f12378d5dd149d9058a1d
Dmitriy Vasilev <raoffonom@icloud.com>
2022-12-18 08:31:41 +0500

    feat(code style): eslint, prettier, absolute import, dotenv

--- Joker/babel.config.js
+++ Joker/babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
---
+++ Joker/src/variables.ts
@@ -0,0 +1,86 @@
+export const LIGHT_TEXT_BASE_1 = '#2E312D';
+export const DARK_TEXT_BASE_1 = '#FFFFFF';
+
+export const LIGHT_TEXT_BASE_2 = '#8E8E8E';
+export const DARK_TEXT_BASE_2 = '#8E8E8E';
+
+export const LIGHT_TEXT_BASE_3 = '#FFFFFF';
+export const DARK_TEXT_BASE_3 = '#FFFFFF';
+
+export const LIGHT_TEXT_SECOND_1 = '#B2B4BB';
+export const DARK_TEXT_SECOND_1 = '#5E6061';
+
+export const LIGHT_TEXT_SECOND_2 = 'rgba(255, 255, 255, 0.8)';
+export const DARK_TEXT_SECOND_2 = 'rgba(255, 255, 255, 0.8)';
+
+export const LIGHT_TEXT_GREEN_1 = '#01B26E';
+export const DARK_TEXT_GREEN_1 = '#01B26E';
+
+export const LIGHT_TEXT_RED_1 = '#E16363';
+export const DARK_TEXT_RED_1 = '#E16363';
+
+export const LIGHT_TEXT_YELLOW_1 = '#B26F1D';
+export const DARK_TEXT_YELLOW_1 = '#EB9226';
+
+export const LIGHT_TEXT_BLUE_1 = '#007AFF';
+export const DARK_TEXT_BLUE_1 = '#007AFF';
+
+export const LIGHT_GRAPHIC_BASE_1 = '#2F2F2F';
+export const DARK_GRAPHIC_BASE_1 = '#FFFFFF';
+
+export const LIGHT_GRAPHIC_BASE_2 = '#8E8E8E';
+export const DARK_GRAPHIC_BASE_2 = '#8E8E8E';
+
+export const LIGHT_GRAPHIC_BASE_3 = '#FFFFFF';
+export const DARK_GRAPHIC_BASE_3 = '#FFFFFF';
+
+export const LIGHT_GRAPHIC_RED_1 = '#E16363';
+export const DARK_GRAPHIC_RED_1 = '#E16363';
+
+export const LIGHT_GRAPHIC_GREEN_1 = '#01B26E';
+export const DARK_GRAPHIC_GREEN_1 = '#01B26E';
+
+export const LIGHT_GRAPHIC_GREEN_2 = '#04D484';
+export const DARK_GRAPHIC_GREEN_2 = '#04D484';
+
+export const LIGHT_GRAPHIC_SECOND_1 = '#EFEFEF';
+export const DARK_GRAPHIC_SECOND_1 = '#2C3231';
+
+export const LIGHT_GRAPHIC_SECOND_2 = '#CFD1DB';
+export const DARK_GRAPHIC_SECOND_2 = '#56575C';
+
+export const LIGHT_GRAPHIC_SECOND_3 = '#CCCDD2';
+export const DARK_GRAPHIC_SECOND_3 = '#454647';
+
+export const LIGHT_GRAPHIC_SECOND_4 = '#AAABB2';
+export const DARK_GRAPHIC_SECOND_4 = '#616266';
+
+export const LIGHT_BG_1 = '#FFFFFF';
+export const DARK_BG_1 = '#181C1A';
+
+export const LIGHT_BG_2 = '#EEF9F5';
+export const DARK_BG_2 = '#25332E';
+
+export const LIGHT_BG_3 = '#F4F8F8';
+export const DARK_BG_3 = '#212624';
+
+export const LIGHT_BG_4 = 'rgba(4, 212, 132, 0.5)';
+export const DARK_BG_4 = 'rgba(4, 212, 132, 0.5)';
+
+export const LIGHT_BG_5 = 'rgba(225, 99, 99, 0.8)';
+export const DARK_BG_5 = 'rgba(225, 99, 99, 0.8)';
+
+export const LIGHT_BG_6 = '#FCEDCE';
+export const DARK_BG_6 = '#48361B';
+
+export const LIGHT_BG_7 = '#F9EEEE';
+export const DARK_BG_7 = '#F9EEEE';
+
+export const LIGHT_BG_8 = '#F4F5F8';
+export const DARK_BG_8 = '#292E2D';
+
+export const LIGHT_BG_9 = 'rgba(0, 0, 0, 0.6)';
+export const DARK_BG_9 = 'rgba(0, 0, 0, 0.6)';
+
+export const LIGHT_BG_10 = '#181C1A';
+export const DARK_BG_10 = '#181C1A';
