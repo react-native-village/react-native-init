f482f019a554c2d28d6039e5b43f1a28fa257390
Max <maskalyov.maks@gmail.com>
2022-12-21 11:42:16 +0300

    add test text component

--- Joker/src/screens/tab-screen1.tsx
+++ Joker/src/screens/tab-screen1.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import {Button, StyleSheet, View} from 'react-native';
 
+import {Color} from 'src/colors';
+import {Text} from 'src/components/ui';
 import {useTypedNavigation} from 'src/hooks';
 import {app} from 'src/services';
 import {LIGHT_BG_1} from 'src/variables';
@@ -28,6 +30,9 @@ export function TabScreen1() {
           app.githubAuth.logout();
         }}
       />
+      <View style={styles.test}>
+        <Text color={Color.graphicGreen1}>Test theme hook</Text>
+      </View>
     </View>
   );
 }
@@ -38,5 +43,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: LIGHT_BG_1,
+  },
+  test: {
+    width: 200,
+    height: 200,
+    backgroundColor: Color.graphicBase1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
