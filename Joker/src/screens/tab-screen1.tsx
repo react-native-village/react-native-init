67754175464beb5a447263409dca9e06f0b6e740
Max <maskalyov.maks@gmail.com>
2022-12-25 15:08:26 +0300

    refactor

--- Joker/src/screens/tab-screen1.tsx
+++ Joker/src/screens/tab-screen1.tsx
@@ -4,17 +4,18 @@ import {Button, StyleSheet, View} from 'react-native';
 
 import {Text} from 'src/components/ui';
 import {showLoadingWithText} from 'src/helpers';
-import {useTypedNavigation} from 'src/hooks';
+import {useThemeObject, useTypedNavigation} from 'src/hooks';
 import {useTheme} from 'src/hooks/use-theme';
 import {app} from 'src/services';
-import {Theme} from 'src/themes';
+import {ColorTheme} from 'src/themes';
 
 export function TabScreen1() {
-  const {theme, lightTheme, darkTheme, systemTheme} = useTheme();
-  const Styles = React.useMemo(() => createStyles(theme), [theme]);
+  const {lightTheme, darkTheme, systemTheme} = useTheme();
+  const {color} = useTheme().theme;
+  const styles = useThemeObject(createStyles);
   const naviagtion = useTypedNavigation();
   return (
-    <View style={Styles.container}>
+    <View style={styles.container}>
       <Button
         title="go to first screen for modal"
         onPress={() => {
@@ -33,9 +34,9 @@ export function TabScreen1() {
           app.githubAuth.logout();
         }}
       />
-      <View style={Styles.test}>
-        <Text color={theme.color.textGreen1}>Test theme hook</Text>
-        <Text color={theme.color.textYellow1}>Choose Theme</Text>
+      <View style={styles.test}>
+        <Text color={color.textBase1}>Test theme hook</Text>
+        <Text color={color.textYellow1}>Choose Theme</Text>
         <Button title={'Dark'} onPress={darkTheme} />
         <Button title={'Light'} onPress={lightTheme} />
         <Button title={'System'} onPress={systemTheme} />
@@ -48,22 +49,20 @@ export function TabScreen1() {
   );
 }
 
-const createStyles = (theme: Theme) => {
+const createStyles = (color: ColorTheme) => {
   const styles = StyleSheet.create({
-    // eslint-disable-next-line react-native/no-unused-styles
     container: {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: theme.color.bg1,
+      backgroundColor: color.bg1,
     },
-    // eslint-disable-next-line react-native/no-unused-styles
     test: {
       width: 200,
       height: 200,
       alignItems: 'center',
       justifyContent: 'center',
-      backgroundColor: theme.color.graphicBase1,
+      backgroundColor: color.graphicBase2,
     },
   });
   return styles;
