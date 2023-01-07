d1eff82141f576cf73ab829f07312abd1623c95e
Dmitriy Vasilev <raoffonom@icloud.com>
2023-01-07 13:25:13 +0500

    chai expect

--- joker-moralis-server/src/contracts/tests/Token.test.ts
+++ joker-moralis-server/src/contracts/tests/Token.test.ts
@@ -1,8 +1,8 @@
 import {ethers} from 'hardhat';
+import {expect} from 'chai';
 
 describe('Token contract', function () {
   it('Deployment should assign the total supply of tokens to the owner', async function () {
-    expect.assertions(1);
     const [owner] = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory('Token');
@@ -10,10 +10,9 @@ describe('Token contract', function () {
     const hardhatToken = await Token.deploy();
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).toEqual(ownerBalance);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
   it('Should transfer tokens between accounts', async function () {
-    expect.assertions(2);
     const [, addr1, addr2] = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory('Token');
@@ -22,14 +21,10 @@ describe('Token contract', function () {
 
     // Transfer 50 tokens from owner to addr1
     await hardhatToken.transfer(addr1.address, 50);
-    expect(
-      parseInt(await (await hardhatToken.balanceOf(addr1.address))._hex),
-    ).toEqual(50);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
 
     // // Transfer 50 tokens from addr1 to addr2
     await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-    expect(
-      parseInt(await (await hardhatToken.balanceOf(addr2.address))._hex),
-    ).toEqual(50);
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
   });
 });
