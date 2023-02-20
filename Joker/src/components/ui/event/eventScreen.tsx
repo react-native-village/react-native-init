7dabfff2ad310bf4007c9a1f4d39303c5461163a
Max <maskalyov.maks@gmail.com>
2023-02-20 15:43:54 +0300

    work

--- Joker/src/components/ui/event/eventScreen.tsx
+++ Joker/src/components/ui/event/eventScreen.tsx
@@ -23,7 +23,7 @@ export function EventScreen({id = '1', pressBack}: EventScreenProps) {
   const event = DATA.find(item => item.id === id);
   const price = 300;
   const currencySymbols = 'ETH';
-  const {styles} = useThematicStyles(useStyles);
+  const {styles} = useThematicStyles(rawStyles);
   return (
     <Background style={styles.container}>
       <Image source={{uri: event?.imageUrl}} style={styles.image} />
@@ -36,18 +36,43 @@ export function EventScreen({id = '1', pressBack}: EventScreenProps) {
           <View style={styles.rowTicket}>
             <MaterialCommunityIcons
               name={'ticket-confirmation-outline'}
-              style={styles.iconStyle}
+              style={styles.iconStyle1}
             />
             <Text t11>One time usage</Text>
           </View>
-          <Text t20 color={Color.primary}>{`${price} ${currencySymbols}`}</Text>
+          <View style={styles.price}>
+            <Text t20 color={Color.primary1}>
+              {`${price} ${currencySymbols}`}
+            </Text>
+            <Text t12 color={Color.graphicSecond4}>
+              (19,564213$)
+            </Text>
+          </View>
+        </View>
+        <View style={styles.rowInfo}>
+          <View style={styles.circle}>
+            <MaterialCommunityIcons
+              name="calendar-check"
+              style={styles.iconStyle2}
+            />
+          </View>
+          <View>
+            <Text t7 style={styles.dateText}>
+              Start Date: {event?.startData}
+            </Text>
+            <Text t9>Saturday, 4:00 PM</Text>
+            <Text t7 style={styles.dateText}>
+              End Date: {event?.endData}
+            </Text>
+            <Text t9>Saturday, 10:00 PM</Text>
+          </View>
         </View>
       </View>
     </Background>
   );
 }
 
-const useStyles = StyleSheet.create({
+const rawStyles = StyleSheet.create({
   container: {
     flex: 1,
   },
@@ -57,10 +82,17 @@ const useStyles = StyleSheet.create({
     resizeMode: 'cover',
     marginBottom: 25,
   },
-  iconStyle: {
+  iconStyle1: {
     color: Color.textSecond1,
     marginRight: 10,
+    fontSize: 24,
+  },
+  iconStyle2: {
+    color: Color.primary2,
     fontSize: 30,
+  },
+  dateText: {
+    marginBottom: 5,
   },
   details: {
     paddingHorizontal: 24,
@@ -71,9 +103,28 @@ const useStyles = StyleSheet.create({
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: 45,
   },
   rowTicket: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  price: {
+    alignItems: 'center',
+  },
+  rowInfo: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  circle: {
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 20,
+    borderRadius: 100,
+    backgroundColor: Color.primary3,
   },
 });
