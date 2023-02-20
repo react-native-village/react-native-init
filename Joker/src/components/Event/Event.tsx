856e29be74fd434e89a5d4dbaae451cd674a0f6f
Max <maskalyov.maks@gmail.com>
2023-02-20 18:35:42 +0300

    preFinal changes

--- Joker/src/components/Event/Event.tsx
+++ Joker/src/components/Event/Event.tsx
@@ -1,73 +1,130 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Dimensions, Image, StyleSheet, View} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import {useThematicStyles} from 'src/hooks';
 import {Color} from 'src/themeTypes';
 
+import {EventBuy} from './EventBuy';
 import {EventTags} from './EventTags';
 
 import {DATA} from '../HomeMarket';
 import {Background} from '../ui/Background';
 import {Text} from '../ui/text/text';
 
+const widthScreen = Dimensions.get('screen').width;
 interface EventScreenProps {
   id?: string;
   pressBack?: () => void;
 }
 
-const widthScreen = Dimensions.get('screen').width;
-
 export function Event({id = '1'}: EventScreenProps) {
+  const [showBuy, setShowBuy] = useState(false);
+  const insets = useSafeAreaInsets();
   const event = DATA.find(item => item.id === id);
   const price = 300;
   const currencySymbols = 'ETH';
   const {styles} = useThematicStyles(rawStyles);
   return (
-    <Background style={styles.container}>
-      <Image source={{uri: event?.imageUrl}} style={styles.image} />
-      <View style={styles.details}>
-        <EventTags tags={event!.tags} />
-        <Text t19 style={styles.name}>
-          {event?.name}
-        </Text>
-        <View style={styles.row}>
-          <View style={styles.rowTicket}>
-            <MaterialCommunityIcons
-              name={'ticket-confirmation-outline'}
-              style={styles.iconStyle1}
-            />
-            <Text t11>One time usage</Text>
+    <Background
+      style={{
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+      }}>
+      <ScrollView style={styles.container}>
+        <Image source={{uri: event?.imageUrl}} style={styles.image} />
+        <View style={styles.details}>
+          <EventTags tags={event!.tags} />
+          <Text t19 style={styles.name}>
+            {event?.name}
+          </Text>
+          <View style={styles.row}>
+            <View style={styles.rowTicket}>
+              <MaterialCommunityIcons
+                name={'ticket-confirmation-outline'}
+                style={styles.iconStyle1}
+              />
+              <Text t11>One time usage</Text>
+            </View>
+            <View style={styles.price}>
+              <Text t20 color={Color.primary1}>
+                {`${price} ${currencySymbols}`}
+              </Text>
+              <Text t12 color={Color.graphicSecond4}>
+                (19,564213$)
+              </Text>
+            </View>
           </View>
-          <View style={styles.price}>
-            <Text t20 color={Color.primary1}>
-              {`${price} ${currencySymbols}`}
-            </Text>
-            <Text t12 color={Color.graphicSecond4}>
-              (19,564213$)
-            </Text>
+          <View style={styles.rowInfo}>
+            <View style={styles.circle}>
+              <MaterialCommunityIcons
+                name="calendar-check"
+                style={styles.iconStyle2}
+              />
+            </View>
+            <View>
+              <Text t7 style={styles.dateText}>
+                Start Date: {event?.startData}
+              </Text>
+              <Text t9>Saturday, 4:00 PM</Text>
+              <Text t7 style={styles.dateText}>
+                End Date: {event?.endData}
+              </Text>
+              <Text t9>Saturday, 10:00 PM</Text>
+            </View>
           </View>
+          <View style={styles.rowInfo}>
+            <View style={styles.circle}>
+              <MaterialCommunityIcons
+                name="map-marker-outline"
+                style={styles.iconStyle2}
+              />
+            </View>
+            <View>
+              <Text t7 style={styles.dateText}>
+                Location
+              </Text>
+              <Text t9>{event?.geoPosition}</Text>
+            </View>
+          </View>
+          <View style={styles.rowInfo}>
+            <View style={styles.circle}>
+              <MaterialCommunityIcons
+                name="account"
+                style={styles.iconStyle2}
+              />
+            </View>
+            <View>
+              <Text t7 style={styles.dateText}>
+                Ticket Provider
+              </Text>
+              <Text t9>0x0da46c783f8cxv85x6z5cxhxv12382</Text>
+              <Text t7 style={styles.dateText}>
+                Ticket Approval
+              </Text>
+              <Text t9>0x0cd46a783f8cxv45x6z5cxhxv13782</Text>
+            </View>
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setShowBuy(true)}
+            activeOpacity={0.6}>
+            <Text t4 color={Color.textBase3}>
+              Buy Ticket
+            </Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.rowInfo}>
-          <View style={styles.circle}>
-            <MaterialCommunityIcons
-              name="calendar-check"
-              style={styles.iconStyle2}
-            />
-          </View>
-          <View>
-            <Text t7 style={styles.dateText}>
-              Start Date: {event?.startData}
-            </Text>
-            <Text t9>Saturday, 4:00 PM</Text>
-            <Text t7 style={styles.dateText}>
-              End Date: {event?.endData}
-            </Text>
-            <Text t9>Saturday, 10:00 PM</Text>
-          </View>
-        </View>
-      </View>
+      </ScrollView>
+      {showBuy && <EventBuy onClose={() => setShowBuy(false)} />}
     </Background>
   );
 }
@@ -75,6 +132,16 @@ export function Event({id = '1'}: EventScreenProps) {
 const rawStyles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  button: {
+    backgroundColor: Color.primary2,
+    borderRadius: 100,
+    width: '100%',
+    height: 55,
+    marginHorizontal: 1,
+    marginVertical: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   image: {
     width: widthScreen,
@@ -117,6 +184,7 @@ const rawStyles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'flex-start',
     justifyContent: 'flex-start',
+    marginBottom: 30,
   },
   circle: {
     width: 60,
